4a2e9dc76a0c65aada837a7b4f35a2049f1a18bd
Richard Virgo Richter <virgo47@gmail.com>
2017-04-26 23:08:37 +0200

    initial version of experimental rest app (spring boot) and Swagger doc UI

---
+++ src/main/java/restapidemo/MethodCheckerMain.java
@@ -0,0 +1,12 @@
+package restapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MethodCheckerMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MethodCheckerMain.class, args);
+	}
+}
