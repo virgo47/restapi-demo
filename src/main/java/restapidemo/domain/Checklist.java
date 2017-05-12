5eb9f1e8be11187d254703edd39d9463724e0aff
richter <richard.richter@posam.sk>
2017-05-12 16:26:55 +0200

    documenting /box (not finished)

--- src/main/java/restapidemo/domain/Checklist.java
+++
@@ -1,8 +0,0 @@
-package restapidemo.domain;
-
-/**
- * Checklist is a list of items, potentially across various boxes. Items can be in any number
- * of checklists.
- */
-public class Checklist {
-}
---
+++ src/main/java/restapidemo/domain/Label.java
@@ -0,0 +1,9 @@
+package restapidemo.domain;
+
+/**
+ * Label can be attached to an item. The same label can be attached to many items across
+ * various boxes. Items can be marked with any number of labels. (Another concept used
+ * in the same manner is called "tags".)
+ */
+public class Label {
+}
